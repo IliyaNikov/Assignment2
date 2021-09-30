0ad88e717a54b31f29d69a7af8d53051f33c9bcc
IliyaNikov <iliya.nikov@gmail.com>
2021-09-30 11:26:08 +0200

    fixed bugs, adults and login work

--- BlazorClientFinal/Data/UserCloudService.cs
+++ BlazorClientFinal/Data/UserCloudService.cs
@@ -12,9 +12,8 @@ namespace BlazorClientFinal.Data
 
         public async Task<User> ValidateUserAsync(string username, string password)
         {
-            HttpClient client = new HttpClient();
+           /* HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync($"http://localhost:5003/User?username={username}&password={password}");
-
             if(response.StatusCode == HttpStatusCode.OK)
             { 
                 string userAsJson = await response.Content.ReadAsStringAsync();
@@ -22,21 +21,20 @@ namespace BlazorClientFinal.Data
                 User result = JsonSerializer.Deserialize<User>(userAsJson);
                 return result;
             }
-                
-            throw new Exception("User not found"); 
+            throw new Exception("User not found"); */
            
            
-           /* Hardcoded user. It works! 
+           //Hardcoded user. It works! 
            User tryout = new User();
            tryout.password = "12345";
            tryout.username = "Troels";
            tryout.SecurityLevel = "1";
-           if (username == tryout.username && passwordExpected == tryout.password)
+           if (username == tryout.username && password == tryout.password)
            {
                return tryout;
            }
            throw new Exception("The login name or password is not correct");
-           */
+           
         }
     }
 }
